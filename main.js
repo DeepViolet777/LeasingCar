ae79b36aee7dc3abb8bce007d81033cc5ffc8c8d
Anna Malorodova <angervenus6@gmail.com>
2021-04-20 10:28:30 +0300

    Inicial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.slick-slider').slick({
+      slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  infinite:true,
+  fade: true,
+  cssEase: 'linear',
+  speed: 500,
+  swipeToSlide: true,
+    });
+  });

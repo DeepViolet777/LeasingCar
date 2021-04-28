8c15570ac71919367d3866e8c92b0374a7ac9bd1
Anna Malorodova <angervenus6@gmail.com>
2021-04-28 13:32:45 +0300

    Range plagin

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 $(document).ready(function(){
     $('.slick-slider').slick({
       slidesToShow: 1,
@@ -10,4 +11,46 @@ $(document).ready(function(){
   speed: 500,
   swipeToSlide: true,
     });
+
+   /* var mySlider = document.
+    rangesliderJs.create(mySlider);
+
+    rangesliderJs.create(mySlider, {
+      onInit: (value, percent, position) => {},
+      onSlideStart: (value, percent, position) => {},
+      onSlide: (value, percent, position) => {},
+      onSlideEnd: (value, percent, position) => {}
+    });*/
+
+    var rangeOne = document.getElementByClassName('range_container');
+
+noUiSlider.create(rangeOne, {
+    start: [1000000],
+    connect: 'lower',
+    range: {
+        'min': [1000000],
+        'max': [6000000]
+    }
+});
+
   });
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

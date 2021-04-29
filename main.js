be73c59a6a1356b11798f07b75353e19e56218f2
Anna Malorodova <angervenus6@gmail.com>
2021-04-29 16:00:11 +0300

    Inputs and ranges synchronized

--- main.js
+++ main.js
@@ -1,68 +1,79 @@
 
 $(document).ready(function(){
     $('.slick-slider').slick({
-      slidesToShow: 1,
-  slidesToScroll: 1,
-  dots: true,
-  infinite:true,
-  fade: true,
-  cssEase: 'linear',
-  speed: 500,
-  swipeToSlide: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite:true,
+        fade: true,
+        cssEase: 'linear',
+        speed: 500,
+        swipeToSlide: true,
     });
 
-   /* var mySlider = document.
-    rangesliderJs.create(mySlider);
 
-    rangesliderJs.create(mySlider, {
-      onInit: (value, percent, position) => {},
-      onSlideStart: (value, percent, position) => {},
-      onSlide: (value, percent, position) => {},
-      onSlideEnd: (value, percent, position) => {}
-    });*/
-  });
+    /*
+    Передаем селекторы, и данные для рейнджа.
+    Находим элементы, на блоке для рейнджа создаем noUiSlider.
+    На слайдер вешаем событие update, по нему обновляем value нашего инпута.
+    На сам инпут вешаем установку значения слайдеру по blur.
+    Возвращаем объект со элементами слайдера и инпута для удобства. В первом уже есть noUiSlider.
+    Обработка значений, выходящих за крайние, у noUiSlider имеется из коробки.
+    Фыр!
+    */
+    const initRange = (rangeSelector, inputSelector, range) => {
+        const rangeElem = document.querySelector(rangeSelector);
+        const inputElem = document.querySelector(inputSelector);
 
-    const carCostRange = document.querySelector('.car-cost_range')
+        if (!rangeElem || !inputElem) return;
 
-    const initFeeRange = document.querySelector('.init-fee_range')
+        noUiSlider.create(rangeElem, {
+            start: range.min,
+            connect: 'lower',
+            range,
+            format: {
+                to: val => Math.round(Number(val)),
+                from: val => Number(val),
+            }
+        });
 
-    const leaseTermRange = document.querySelector('.lease-term_range')
+        rangeElem.noUiSlider.on('update', () => {
+            inputElem.value = rangeElem.noUiSlider.get();
+        })
 
-noUiSlider.create(carCostRange, {
-    start: [1000000],
-    connect: 'lower',
-    range: {
-        'min': [1000000],
-        'max': [6000000]
-    }
+        inputElem.addEventListener('blur', e => {
+            rangeElem.noUiSlider.set(Number(e.target.value));
+        });
 
- 
+        return {rangeElem, inputElem};
+    };
 
-  });
+    const carCostRange = initRange(
+        '.car-cost_range',
+        '.car-cost_num',
+        {
+            min: [1000000],
+            max: [6000000],
+        });
 
-  noUiSlider.create(initFeeRange, {
-    start: [1000000],
-    connect: 'lower',
-    range: {
-        'min': [1000000],
-        'max': [6000000]
-    }
+    const initFeeRange = initRange(
+        '.init-fee_range',
+        '.init-fee_percent',
+        {
+            min: [10],
+            max: [60]
+        });
 
-  });
+    const leaseTermRange = initRange(
+        '.lease-term_range',
+        '.lease-term_num',
+        {
+            min: [1],
+            max: [60]
+        });
 
 
-  noUiSlider.create(leaseTermRange, {
-    start: [1000000],
-    connect: 'lower',
-    range: {
-        'min': [1000000],
-        'max': [6000000]
-    }
-
- 
-
-  });
-
+});
 
 
 
